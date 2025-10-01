e8fc19eea14938310da8cc9d5818fdf998687a11
guigarher <guigarher@gmail.com>
2025-10-01 11:54:00 +0100

    asd

--- 01-10/videojuego/main.js
+++ 01-10/videojuego/main.js
@@ -1,4 +1,26 @@
 // Importamos las clases
-import { Jugador } from './personaje.js';
+import { Personaje } from './personaje.js';
 import { Enemigo } from './enemigo.js';
 import { Objeto } from './objeto.js';
+
+const jugador = new Personaje('Heroe', 100, 20);
+const enemigo = new Enemigo('Goblin', 50, 10);
+const pocion = new Objeto('Poción de Vida', 'cura 20 puntos de vida');
+const espada = new Objeto('Espada Corta', 'arma');
+
+// Simulación de acciones
+console.log('--- Inicio del Juego ---');
+jugador.recoger(pocion);
+jugador.recoger(espada);
+
+jugador.mostrarEstado();
+enemigo.mostrarEstado();
+
+jugador.atacar(enemigo);
+enemigo.atacar(jugador);
+
+pocion.usar(jugador);
+
+jugador.mostrarEstado();
+enemigo.mostrarEstado();
+console.log('--- Fin del Juego ---');
