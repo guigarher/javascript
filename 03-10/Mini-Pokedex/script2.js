b2cc1ad06c3734a0622070c77f2cae3fdceb6281
guigarher <guigarher@gmail.com>
2025-10-17 10:43:50 +0100

    asd

--- 03-10/Mini-Pokedex/script2.js
+++ 03-10/Mini-Pokedex/script2.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-// =======================
-// 0) REFERENCIAS AL DOM
-// =======================
-const pokedex      = document.getElementById("pokedex");
-const estado       = document.getElementById("estado");
-const inputCantidad= document.getElementById("cantidad");
-const btnMostrar   = document.getElementById("mostrar");
-const inputId      = document.getElementById("pokeId");
-const btnMostrarId = document.getElementById("mostrarId");
-
-// --- 1) TRAER UN POKÉMON ---
-async function getPokemon(id) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  if (!res.ok) throw new Error(`HTTP ${res.status} (id ${id})`);
-  const data = await res.json();
-  return data;
-}
-async function getCharmander(){
-  let charmander = await getPokemon(4);
-  }
-async function loadPokemons(){
-  for(let i = 1; i =10; i++){
-    let pokemons = await getPokemon(i);
-  }
-}
-let charmander = getCharmander();
-console.log(charmander);
-
-let mostrarPokemons = loadPokemons();
-console.log(mostrarPokemons);
-
-
-=======
 //==============================
 //REFERENCIAS AL DOM
 //==============================
@@ -127,4 +93,4 @@ async function mostrarUno(){
 //BOTON MOSTRAR 1 POKEMON
 //==============================
 btnMostrarUno.addEventListener("click", mostrarUno);
->>>>>>> 0cc2a04c22d058690f419c6c06477d31275462c6
+
